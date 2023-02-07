b7d4dad63d8b07f9443a6578de05ef2af476840b
John Rey <adioskid@mail.ru>
2023-02-07 14:26:18 +0300

    API

--- src/Sup.java
+++ src/Sup.java
@@ -1,4 +1,20 @@
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class Sup {
+
+    int test() {
+        return (int) 12.1;
+    }
+
+    static Supplier<? extends Integer> state() {
+        return () -> (int) (Math.random() * 11);
+
+    }
+
     public static void main(String[] args) {
+        Stream<Integer> integerStream = Stream.generate(state());
+        integerStream.limit(1).forEach(System.out::println);
     }
 }
